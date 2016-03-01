e3c3644a0acd40a2e797cecb3b87ae5eaa06e789
asell22 <alexanderjsell@gmail.com>
2016-03-01 23:49:59 +0000

    public folder to serve static files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.use(express.static('public'));
+
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log('server running on', process.env.PORT); 
 });

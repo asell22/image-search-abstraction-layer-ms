c5fdee931694ee2d6c2e182cdfeb0f1bc3e9bc3d
asell22 <alexanderjsell@gmail.com>
2016-03-02 19:15:57 +0000

    added '/api/latest' route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var request = require('request');
 require('dotenv').config();
+var latest = [];
 
 
 app.use(express.static('public'));
@@ -9,6 +10,11 @@ app.use(express.static('public'));
 app.get('/api/imagesearch/:query', function(req, res) {
     var imageJSON = []
     var query = req.params.query;
+    var queryObj = {
+        term: query,
+        date: new Date()
+    };
+    latest.unshift(queryObj);
     var offset = req.query.offset || 1;
     request('https://www.googleapis.com/customsearch/v1?start='+offset+'&key='+process.env.API_KEY+'&cx='+process.env.ENGINE_ID+'&q='+query+'&searchType=image&fileType=jpg&alt=json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -25,6 +31,10 @@ app.get('/api/imagesearch/:query', function(req, res) {
          res.send(imageJSON);
     });
    
+});
+
+app.get('/api/latest', function(req, res) {
+    res.send(latest);
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
